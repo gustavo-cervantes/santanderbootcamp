0a1112e714c386b244200840d172005099b52434
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-05-07 16:04:20 -0300

    uso de strings

--- firstprojectjava/src/MinhaClasse.java
+++ firstprojectjava/src/MinhaClasse.java
@@ -1,9 +1,17 @@
 public class MinhaClasse {
     
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         
-        System.out.print("Olá, Mundo!");
+        String primeiroNome = "Gustavo";
+        String segundoNome = "Roldao";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
         
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
