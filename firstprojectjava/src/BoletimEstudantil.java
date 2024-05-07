a38aed5044e888c6a860a11758c16dd1a5a98402
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-05-07 16:20:57 -0300

    anatomia de classes/identação

---
+++ firstprojectjava/src/BoletimEstudantil.java
@@ -0,0 +1,28 @@
+public class BoletimEstudantil {
+
+
+    /** 
+     *  CÓDIGO SEM IDENTAÇÃO ! ! !
+     * public static void main(String[] args) {
+        int mediaFinal = 6;
+        if(mediaFinal<6)
+        System.out.println("REPROVADO");
+        else if(mediaFinal==6)
+        System.out.println("PROVA MINERVA");
+        else
+        System.out.println("APROVADO");
+    }
+        CÓDIGO IDENTADO ABAIXO ! ! !
+    */ 
+
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+
+}
