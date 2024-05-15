bbfe93ac73dc29178b80de5a2968ce910cb76c06
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-05-15 17:14:06 -0300

    condicional composta

---
+++ firstprojectjava/src/edu/gustavo/controlefluxo/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+package edu.gustavo.controlefluxo;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        
+        int nota = 8;
+
+        if (nota >=7)
+        System.out.println("Aluno aprovado!");
+
+        else
+        System.out.println("Aluno reprovado");
+    }
+}
