f3473b18fdca1ddd84d8e5c9c36a5fd39c366662
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-05-16 14:12:43 -0300

    for each

--- firstprojectjava/src/edu/gustavo/estruturasderep/ExemploForArray.java
+++ firstprojectjava/src/edu/gustavo/estruturasderep/ExemploForArray.java
@@ -6,9 +6,8 @@ public class ExemploForArray {
         // em arrays o indice inicia sempre em 0/ZERO
         String alunos [] = { "FELIPE", "JONAS", "GUSTAVO", "GABRIELA"};
 
-        for (int x=0; x < alunos.length; x++) {
-            
-            System.out.println("O aluno no indice x =" + x + " é " + alunos [ x ]);
+        for (String aluno: alunos) {
+            System.out.println("Nome do aluno é: " + aluno );
         }
     }
 }
