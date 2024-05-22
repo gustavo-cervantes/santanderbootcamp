04d5bd97e771a653f2234750551c3a15b1902546
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-05-22 15:19:26 -0300

    analisando candidatos

--- firstprojectjava/src/edu/gustavo/desafiosdeprojeto/ProcessoSeletivo.java
+++ firstprojectjava/src/edu/gustavo/desafiosdeprojeto/ProcessoSeletivo.java
@@ -1,5 +1,20 @@
 package edu.gustavo.desafiosdeprojeto;
 
 public class ProcessoSeletivo {
-    
+    public static void main(String[] args) {
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");    
+        }else if(salarioBase==salarioPretendido)
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRAPROPOSTA");
+        else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+      
+    }
 }
