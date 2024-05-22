d3ccae0585d00d623b5d1740379d39499f90c4b5
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-05-22 16:50:22 -0300

    uso do for na aplicação

--- firstprojectjava/src/edu/gustavo/desafiosdeprojeto/ProcessoSeletivo.java
+++ firstprojectjava/src/edu/gustavo/desafiosdeprojeto/ProcessoSeletivo.java
@@ -4,9 +4,25 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         selecaoCandidatos();
+        imprimirSelecionados();
+    }
+    static void imprimirSelecionados() {
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice=0; indice < candidatos.length;indice++) {
+            System.out.println("O candidato de nº" + (indice+1) + " é o" + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+
+        for(String candidato: candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
     static void selecaoCandidatos () {
-        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MIRELA","DANIELA","JORGE"};
         
         int candidatosSelecionados = 0;
         int candidatosAtual=0;
