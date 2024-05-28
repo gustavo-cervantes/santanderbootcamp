b6682d0ab692857ee0a4e28e81f6295fa8f29203
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-05-27 21:49:21 -0300

    4 / 5 - verificação de cheque especial

---
+++ santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/VerificacaoChequeEspecial.java
@@ -0,0 +1,27 @@
+package edu.gustavo.desafiosdeprojeto;
+import java.util.Scanner;
+
+public class VerificacaoChequeEspecial {
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+
+        double saldo = scanner.nextDouble(); 
+        double saque = scanner.nextDouble(); 
+        double limiteChequeEspecial = 500; 
+
+        // TODO: Verifique se o saque não ultrapassa o saldo disponível na conta:
+
+        if( saque < saldo ){
+          System.out.println("Transacao realizada com sucesso.");
+        } else if ( saque > saldo ){
+          if( saque >= (saldo +limiteChequeEspecial) ){
+            System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
+          }else {
+            System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+          }
+        }
+
+// Fechamos o objeto Scanner para liberar os recursos:
+        scanner.close(); 
+    }
+}
