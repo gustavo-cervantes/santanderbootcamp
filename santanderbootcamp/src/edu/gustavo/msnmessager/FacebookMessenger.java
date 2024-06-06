1349c8fdbadc0e71bd89717816fc5da22f2dd8ec
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-06-06 11:32:03 -0300

    abstracao

--- santanderbootcamp/src/edu/gustavo/msnmessager/FacebookMessenger.java
+++ santanderbootcamp/src/edu/gustavo/msnmessager/FacebookMessenger.java
@@ -1,4 +1,10 @@
 package edu.gustavo.msnmessager;
 
 public class FacebookMessenger extends ServicoMensagemInstantanea {
+	public void enviarMensagem() {
+		System.out.println("Enviando mensagem pelo Facebook Messenger");
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Facebook Messenger");
+	}
 }
--- santanderbootcamp/src/edu/gustavo/msnmessager/MSNMessenger.java
+++ santanderbootcamp/src/edu/gustavo/msnmessager/MSNMessenger.java
@@ -1,6 +1,11 @@
 package edu.gustavo.msnmessager;
 
     public class MSNMessenger extends ServicoMensagemInstantanea {
-
+        public void enviarMensagem() {
+            System.out.println("Enviando mensagem pelo MSN Messenger");
+            }
+        public void receberMensagem() {
+            System.out.println("Recebendo mensagem pelo MSN Messenger");
+        }
     }
 
