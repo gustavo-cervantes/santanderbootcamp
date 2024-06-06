8922deba3696dfb101b42aa1e08330c523f33abc
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-06-05 22:11:02 -0300

    encapsulamento

--- santanderbootcamp/src/edu/gustavo/pilarespoo/Autodromo.java
+++ santanderbootcamp/src/edu/gustavo/pilarespoo/Autodromo.java
@@ -1,5 +1,8 @@
 package edu.gustavo.pilarespoo;
 
 public class Autodromo {
-    
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        jeep.ligar();
+    }
 }
--- santanderbootcamp/src/edu/gustavo/pilarespoo/Carro.java
+++ santanderbootcamp/src/edu/gustavo/pilarespoo/Carro.java
@@ -1,5 +1,15 @@
 package edu.gustavo.pilarespoo;
 
 public class Carro {
-    
+    public void ligar() {
+        confereCambio();
+        confereCombustivel();
+        System.out.println("Carro ligado");
+    }
+    private void confereCombustivel() {
+        System.out.println("Conferindo combustivel");
+    }
+    private void confereCambio() {
+        System.out.println("Conferindo cambio em P");
+    }
 }
