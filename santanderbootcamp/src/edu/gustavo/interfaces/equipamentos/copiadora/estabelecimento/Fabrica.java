2ca833a0a32fe968b06e6635808ee193cc4d1be4
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-06-06 22:41:30 -0300

    poo

--- santanderbootcamp/src/edu/gustavo/interfaces/equipamentos/copiadora/estabelecimento/Fabrica.java
+++ santanderbootcamp/src/edu/gustavo/interfaces/equipamentos/copiadora/estabelecimento/Fabrica.java
@@ -6,10 +6,14 @@ import edu.gustavo.interfaces.equipamentos.copiadora.impressora.Impressora;
 import edu.gustavo.interfaces.equipamentos.copiadora.impressora.Laserjet;
 import edu.gustavo.interfaces.equipamentos.copiadora.impressora.digitalizadora.Digitalizadora;
 import edu.gustavo.interfaces.equipamentos.copiadora.multifuncional.EquipamentoMultifuncional;
+import edu.gustavo.interfaces.equipamentos.copiadora.impressora.digitalizadora.*;
 
 public class Fabrica {
     public static void main(String[] args) {
         EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+
+         Scanner scanner = new Scanner();
+
         Impressora impressora = em;
         Digitalizadora digitalizadora = em;
         Copiadora copiadora = em;
