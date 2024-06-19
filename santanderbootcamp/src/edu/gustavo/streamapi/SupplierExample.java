652d3c89111fdb06fbfe8a59966c136bca0fb2ab
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-06-18 23:32:55 -0300

    supplier

---
+++ santanderbootcamp/src/edu/gustavo/streamapi/SupplierExample.java
@@ -0,0 +1,21 @@
+package edu.gustavo.streamapi;
+
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class SupplierExample {
+    public static void main(String[] args) {
+      // Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
+      Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
+  
+      // Usar o Supplier para obter uma lista com 5 saudações
+      List<String> listaSaudacoes = Stream.generate(saudacao)
+          .limit(5)
+          .collect(Collectors.toList());
+  
+      // Imprimir as saudações geradas
+      listaSaudacoes.forEach(System.out::println);
+    }
+  }
