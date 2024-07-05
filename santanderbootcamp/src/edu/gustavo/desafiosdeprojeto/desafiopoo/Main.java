0ba113cd9a6a6edee38c9046d01e3e2ef2f641e0
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-07-05 11:48:38 -0300

    main.java | H E P 2

--- santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Main.java
+++ santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Main.java
@@ -1,6 +1,8 @@
 package edu.gustavo.desafiosdeprojeto.desafiopoo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +15,11 @@ public class Main {
         curso2.setTitulo("curso php");
         curso2.setDescricao("descricao curso php");
         curso2.setCargaHoraria(4);
+
+        Conteudo conteudo = new Curso();
+        Conteudo conteudo1 = new Mentoria();
+
+        List<String> palavras = new ArrayList<>(); 
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de java");
