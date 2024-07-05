516d61a0afb92e0f4043da2b0903a8382f832da0
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-07-04 23:58:07 -0300

    desafiopoo | abs e encapsu

--- santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Main.java
+++ santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Main.java
@@ -1,7 +1,28 @@
 package edu.gustavo.desafiosdeprojeto.desafiopoo;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso php");
+        curso2.setDescricao("descricao curso php");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("descricao curso php");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
+
+
 }
--- santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Mentoria.java
+++ santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Mentoria.java
@@ -1,5 +1,44 @@
 package edu.gustavo.desafiosdeprojeto.desafiopoo;
 
+import java.time.LocalDate;
+
 public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+
+    public Mentoria() {
+    }
+
     
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+    @Override
+    public String toString() {
+        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+    }
+
+
+    public void setData(LocalDate now) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setData'");
+    }
+
 }
