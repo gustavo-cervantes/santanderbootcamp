8e0da8049b8cd278d23a2a91629fa8e4c02708c9
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-07-05 11:35:21 -0300

    heranca e polimorfismo | 2

--- santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Mentoria.java
+++ santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Mentoria.java
@@ -3,49 +3,33 @@ package edu.gustavo.desafiosdeprojeto.desafiopoo;
 import java.time.LocalDate;
 
 public class Mentoria extends Conteudo {
-    private String titulo;
-    private String descricao;
     private int cargaHoraria;
-
-
-    public Mentoria() {
-    }
-
+    private LocalDate data;
     
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
-    public void setCargaHoraria(int cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
-    }
-    @Override
-    public String toString() {
-        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
-    }
-
-
-    public void setData(LocalDate now) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setData'");
-    }
-
-
     @Override
     public double calcularXP() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calcularXP'");
+        return XP_PADRAO + 20d;
     }
+    
+    public Mentoria() {
+        
+    }
+
+
+    public LocalDate getData(){
+        return data;
+
+    }
+
+    public void setData(LocalDate data){
+        this.data = data;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao()                                                                                                          + ", data=" + data + "]";
+    }
+
 
 }
