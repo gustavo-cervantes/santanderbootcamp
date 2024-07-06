2b9ed429b5294ff9850a0cfb7a61258029f875c5
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-07-06 11:52:40 -0300

    Herença e Polimorfismo | 3

---
+++ santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Dev.java
@@ -0,0 +1,11 @@
+package edu.gustavo.desafiosdeprojeto.desafiopoo;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+    
+}
--- santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Main.java
+++ santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafiopoo/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Curso curso1 = new Curso();
+        Curso curso1 = new Curso ();
         curso1.setTitulo("curso java");
         curso1.setDescricao("descricao curso java");
         curso1.setCargaHoraria(8);
