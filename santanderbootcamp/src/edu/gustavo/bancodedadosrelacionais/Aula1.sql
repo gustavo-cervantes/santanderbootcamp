f74d3cd8fb3595f056aea8cc277349e9aaaa315e
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-07-17 22:37:05 -0300

    aula sql | 2

--- santanderbootcamp/src/edu/gustavo/bancodedadosrelacionais/Aula1.sql
+++ santanderbootcamp/src/edu/gustavo/bancodedadosrelacionais/Aula1.sql
@@ -20,9 +20,14 @@ CREATE TABLE viagens.reservas (
   status VARCHAR(255) DEFAULT 'pendente' COMMENT 'Status da reserva (confirmada, pendente, cancelada, etc.)'
 );
 
-
+-- INSERTS --
 INSERT INTO viagens.usuarios (id, nome, email, data_nascimento, endereco) VALUES 
 (1, 'João Silva', 'joao@example.com', '1990-05-15', 'Rua A, 123, Cidade X, Estado Y'),
 (2, 'Maria Santos', 'maria@example.com', '1985-08-22', 'Rua B, 456, Cidade Y, Estado Z'),
 (3, 'Pedro Souza', 'pedro@example.com', '1998-02-10', 'Avenida C, 789, Cidade X, Estado Y');
 (4, 'Gustavo', 'gustavo@example.com', '2006-05-29', 'Rua A, 123, Cidade 1, Estado 2');
+
+INSERT INTO viagens.destinos (id, nome, descricao) VALUES 
+(1, 'Praia da cal', 'Uma das belas praias do litoral gaúcho'),
+(2, 'Prainha', 'Uma praia familiar, e aconchegante, perfeita para tomar um chimarrão em família'),
+(3, 'Orla', 'Uma orla gastronômica com paisagem para o rio mampituba, repleta de opções culinárias.');
