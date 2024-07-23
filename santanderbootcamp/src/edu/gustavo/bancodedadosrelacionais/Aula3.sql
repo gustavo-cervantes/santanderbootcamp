50a316b61b9f2f2d55fe0c61ffc9b34b4438d67b
Gustavo Roldão <gustavodsroldao@gmail.com>
2024-07-23 10:02:03 -0300

    aula 3 |

---
+++ santanderbootcamp/src/edu/gustavo/bancodedadosrelacionais/Aula3.sql
@@ -0,0 +1,18 @@
+
+-- Primary Key--
+-- Tabela "usuarios"
+ALTER TABLE usuarios
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Tabela "destinos"
+ALTER TABLE destinos
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Tabela "reservas"
+ALTER TABLE reservas
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Exemplos --
