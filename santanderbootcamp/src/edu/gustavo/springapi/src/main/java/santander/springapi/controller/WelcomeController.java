e0ae63fc652c4f16278691df4d3faca9b4fed491
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-07-28 14:03:50 -0300

    springboot | projeto rest api 1

---
+++ santanderbootcamp/src/edu/gustavo/springapi/src/main/java/santander/springapi/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package santander.springapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "welcome to my springbootwebapi";
+    }
+}
