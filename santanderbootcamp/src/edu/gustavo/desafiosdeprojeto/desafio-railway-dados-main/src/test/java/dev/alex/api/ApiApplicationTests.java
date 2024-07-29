386215a38a5c92f9db2c0e3b5460b4ae90c1ebd0
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-07-29 18:05:01 -0300

    santander bootcamp finished

--- santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafio-railway-dados-main/src/test/java/dev/alex/api/ApiApplicationTests.java
+++ santanderbootcamp/src/edu/gustavo/desafiosdeprojeto/desafio-railway-dados-main/src/test/java/dev/alex/api/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package springscurity.gustavo;
+package dev.alex.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringsecurityApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
